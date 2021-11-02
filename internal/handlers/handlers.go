622049844111126ddca359539d4acabb6d866c4d
nothingprogram <dev@jongyun.com>
2021-11-03 05:24:05 +0900

    Adding services to a host part4

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -345,9 +346,30 @@ func show500(w http.ResponseWriter, r *http.Request) {
 }
 
 func printTemplateError(w http.ResponseWriter, err error) {
-	_, _ = fmt.Fprint(w, fmt.Sprintf(`<small><span class='text-danger'>Error executing template: %s</span></small>`, err))
+	_, _ = fmt.Fprint(w, fmt.Sprintf(`<small> <span class='text-danger'>Error executing template: %s</span></small>`, err))
+}
+
+type serviceJSON struct {
+	OK bool `json:"ok"`
 }
 
 func (repo *DBRepo) ToggleServiceForHost(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
 
+	hostID, _ := strconv.Atoi(r.Form.Get("host_id"))
+	serviceID, _ := strconv.Atoi(r.Form.Get("service_id"))
+	active, _ := strconv.Atoi(r.Form.Get("active"))
+
+	log.Println("Data: ", hostID, serviceID, active)
+
+	var resp serviceJSON
+	resp.OK = true
+
+	out, _ := json.MarshalIndent(resp, "", "   ")
+	fmt.Println(resp.OK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
